5d7378e063075b57f7c656aa4eb52b5519729fb8
baculadjohnmar <baculadjohnmar@gmail.com>
2025-08-27 14:46:25 +0800

    Add files via upload
    maloi

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        private string brand;
+        private int size;
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+    }
+}
