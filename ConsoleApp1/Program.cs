e65dcc012e3a9a15c85bb286276d8eee98db26b0
baculadjohnmar <baculadjohnmar@gmail.com>
2025-08-29 14:40:54 +0800

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a class object
+            Shoe shoe = new Shoe();
+
+            shoe.Brand = "Nike";
+            shoe.Size = 40;
+
+            shoe.DisplayBrand();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Shoe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Shoe
+    {
+        //Declaring the fields
+        private string brand;
+        private int size;
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+        //Method to display
+        public void DisplayBrand()
+        {
+            Console.WriteLine("Enter the brand: \t\t");
+            Console.WriteLine("Enter the size: \t\t");
+        }
+    }
+}
